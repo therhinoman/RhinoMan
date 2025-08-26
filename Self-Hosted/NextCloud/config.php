7f6b9e756c49febb08f68bf4bfd1548f664bdce8
RhinoMan <rhinoman.me@outlook.com>
2025-08-26 17:06:01 +0400

    Update config.php

--- Self-Hosted/NextCloud/config.php
+++ Self-Hosted/NextCloud/config.php
@@ -2,8 +2,7 @@
 // cd /var/lib/docker/volume/[CONTAINERNAME]/_data/config
 // subl config.php
 // sudo docker exec -it nextcloud_redis redis-cli -a mysecurepassword monitor
-// Same as in docker-compose.yml
-);
+
 
 <?php
 $CONFIG = array (
